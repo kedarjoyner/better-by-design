f776e2af53067f50edaaa8139d00cdbeab004059
kedarjoyner <kedarjoyner@gmail.com>
2018-04-22 06:06:00 -0500

    Display event category and type in same list

--- templates/singles/event.php
+++ templates/singles/event.php
@@ -12,9 +12,85 @@ $location    = get_field('location');
 $site        = get_field('website');
 $gallery     = get_field('gallery');
 $events_page = get_field('events_page', 'options');
-
-
 $event_speakers = get_field('event_speakers');
+
+echo '<section class="single_event white">';
+	echo '<article>';
+		echo '<div class="full">';
+		if($image) {
+			echo '<figure>';
+				echo '<img src="'.$image['sizes']['medium'].'" alt="'.$image['alt'].'"/>';
+			echo '</figure>';
+		}
+		echo '<div class="headline">';
+			echo '<h1>'.$title.'</h1>';
+
+
+			// Display categories and event types
+			$types = get_the_terms(get_the_id(), 'event_types');
+			$cats = get_the_terms(get_the_id(), 'event_categories');
+
+			echo '<section class="taxonomy_list">';
+			echo '<ul class="location_types">';
+			if($types) {
+				foreach ($types as $type) { 
+					echo '<li><a href="'.get_term_link( $type ).'">'.$type->name."</a></li>";
+				}
+			} 
+			if ($cats){
+				foreach ($cats as $term) { 
+					echo '<li><a href="'.get_term_link( $term ).'">'.$term->name."</a></li>";
+				}
+			}
+			echo '</ul></section>';
+
+
+		// Display return to events sidebar
+		echo '<h4>';
+			echo $date->format('l, F jS, Y');
+			if($multi_day && $end_date) { echo ' — '.$end_date->format('l, F jS, Y'); }
+		echo '</h4>';
+		echo '</div>';
+		echo '<div class="information">';
+			echo '<div class="contact">';
+				echo '<div class="event_details">';
+					echo '<p class="date">';
+						echo $date->format('F jS');
+						if($multi_day && $end_date) {
+							echo ' — '.$end_date->format(' F jS');
+						}
+					echo '</p>';
+					echo '<p>';
+						echo $date->format('g:i a');
+						if($venue) { echo ' @ '.$venue; }
+					echo '</p>';
+				echo '</div>';
+				get_address('p', get_the_id());
+				echo '<ul class="tags">';
+					get_phone('li', get_the_id());
+					get_email('li', get_the_id(), FALSE);
+					if($site) echo '<li><a href="'.$site['url'].'">Site</a></li>';
+				echo '</ul>';
+				if($events_page) {
+					echo '<a class="button solid return" href="'.$events_page.'">Return to Events</a>';
+				}
+			echo '</div>';
+
+			// Display event description
+			echo '<div class="description">';
+			if($description) {
+				echo $description;
+			}
+			else {
+				$short_description = get_field('short_description');
+				echo '<p>'.$short_description.'</p>';
+			}
+			echo '</div>';
+		echo '</div>';
+	echo '</article>';
+echo '</section>';
+
+// Display speakers
 
 if ( $event_speakers ) {
 
@@ -52,80 +128,6 @@ if ( $event_speakers ) {
 	}
 	echo '</section>';
 	wp_reset_postdata(); // reset post object to global post
-}
-
-echo '<section class="single_event white">';
-	echo '<article>';
-		echo '<div class="full">';
-		if($image) {
-			echo '<figure>';
-				echo '<img src="'.$image['sizes']['medium'].'" alt="'.$image['alt'].'"/>';
-			echo '</figure>';
-		}
-		echo '<div class="headline">';
-			echo '<h1>'.$title.'</h1>';
-			$types = get_the_terms(get_the_id(), 'event_types');
-			if($types) {
-				echo '<section class="taxonomy_list">';
-					echo '<ul class="location_types">';
-						foreach ($types as $type) { echo '<li><a href="'.get_term_link( $type ).'">'.$type->name."</a></li>"; }
-					echo '</ul>';
-				echo '</section>';
-			}
-			echo '<h4>';
-				echo $date->format('l, F jS, Y');
-				if($multi_day && $end_date) { echo ' — '.$end_date->format('l, F jS, Y'); }
-			echo '</h4>';
-		echo '</div>';
-		echo '<div class="information">';
-			echo '<div class="contact">';
-				echo '<div class="event_details">';
-					echo '<p class="date">';
-						echo $date->format('F jS');
-						if($multi_day && $end_date) {
-							echo ' — '.$end_date->format(' F jS');
-						}
-					echo '</p>';
-					echo '<p>';
-						echo $date->format('g:i a');
-						if($venue) { echo ' @ '.$venue; }
-					echo '</p>';
-				echo '</div>';
-				get_address('p', get_the_id());
-				echo '<ul class="tags">';
-					get_phone('li', get_the_id());
-					get_email('li', get_the_id(), FALSE);
-					if($site) echo '<li><a href="'.$site['url'].'">Site</a></li>';
-				echo '</ul>';
-				if($events_page) {
-					echo '<a class="button solid return" href="'.$events_page.'">Return to Events</a>';
-				}
-			echo '</div>';
-
-
-
-			echo '<div class="description">';
-			if($description) {
-				echo $description;
-			}
-			else {
-				$short_description = get_field('short_description');
-				echo '<p>'.$short_description.'</p>';
-			}
-			echo '</div>';
-		echo '</div>';
-	echo '</article>';
-echo '</section>';
-
-
-$cats = get_the_terms(get_the_id(), 'event_categories');
-
-if($cats) {
-	echo '<section class="taxonomy_list">';
-		echo '<ul class="location_types">';
-			foreach ($cats as $term) { echo '<li><a href="'.get_term_link( $term ).'">'.$term->name."</a></li>"; }
-		echo '</ul>';
-	echo '</section>';
 }
 
 
